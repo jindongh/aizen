4acfe76348bb9ad30fd886393247bcefa02ff60d
Ubuntu <ubuntu@ip-172-31-7-133.us-west-2.compute.internal>
2016-08-10 07:47:06 +0000

    Fix typo and it works now

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 #!/bin/env python
+# -*- coding: UTF-8 -*-
 import normalize
+import pandas
 from sklearn.linear_model import LogisticRegression
 
 # 80% of the dataset will be used to do training.
 SRC_CSV='final.users.csv'
-SRC_CSV='users.csv'
-TRAIN_RATIO = 0.9
-info=pandas.read_csv('users.csv', error_bad_lines=False, warn_bad_lines=True)
+#SRC_CSV='users.csv'
+TRAIN_RATIO = 0.8
+print 'Loading data from csv'
+info=pandas.read_csv(SRC_CSV, error_bad_lines=False, warn_bad_lines=True)
+print 'Normalize data'
 info, dicts = normalize.normalize(info)
 
+print 'Create training set'
 train_size = info.shape[0] * TRAIN_RATIO
-# test 100
-train_size = info.shape[0] - 100
+if info.shape[0] - train_size > 100:
+	train_size = info.shape[0] - 100 
+print 'Total: %d train_size:%d' % (info.shape[0], train_size)
 train_set = info.loc[:train_size]
 test_set = info.loc[train_size+1:]
 
-selectors = dicts.otherids + dicts.appids[1:]
-train_target = train_set[dicts.appids[0]]
-test_target = test_set[dicts.appids[0]]
+target_selector = dicts.appids[0]
+target_app='谷歌拼音输入法'
+target_selector = dicts.appnames[target_app]
+dict.appids.remove(target_selector)
+train_selectors = dicts.otherids + dict.appids
+train_target = train_set[target_selector]
+test_target = test_set[target_selector]
 
 model = LogisticRegression()
-model.fit(train_set[selectors], train_set[target])
+print 'Begin training'
+model.fit(train_set[selectors], train_target)
+print 'Begin predict'
 test_result = model.predict(test_set[selectors])
 
-for i in range(len(test_result)):
-	print test_set.loc[i]['id'],test_result.loc[i], test_target.loc[i]
+for a,b,c in zip(test_set.iterrows(), test_target, test_result):
+	print a[0],b,c
--- normalize.py
+++ normalize.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 
 import sets
+import sys
 
 class Dicts:
 	def __init__(self):
@@ -8,43 +9,55 @@ class Dicts:
 		self.appnames={}
 		self.otherids=[]
 		self.othernames={}
-	def add_name(self, name, value):
-		if name in self.othernames:
+	def add_name(self, field_type, field_name, value):
+		field_key = field_type + "." + field_name
+		if field_key in self.othernames:
 			raise 'Duplicate name: %s' % name
-		self.othernames[name] = value
+		self.othernames[field_key] = value
 	def add_appname(self, name, value):
 		if name in self.appnames:
 			raise 'Duplicate app name: %s' % name
 		self.appnames[name] = value
 def normalize(ds):
 	dicts=Dicts()
+	print '\tNormalize device'
 	ds = normalize_field(ds, 'device', dicts)
+	print '\tNormalize phoneinfo'
 	ds = normalize_field(ds, 'phoneinfo', dicts)
+	print '\tNormalize isroot'
 	ds = normalize_field(ds, 'isroot', dicts, {'1':1,1:1,'0':0,0:0})
+	print '\tNormalize nettype'
 	ds = normalize_field(ds, 'nettype', dicts, {'1':1,1:1,'0':0,0:0})
+	print '\tNormalize addr'
 	ds = normalize_field(ds, 'addr', dicts)
+	print '\tPrepare normalize appNames'
 	appNameSet=sets.Set()
-	ds['apps'].apply(lambda x : appNameSet.update(x.split(',')))
+	ds['appNames'].apply(lambda x : appNameSet.update(x.split(',')))
 	appId=0
+	print '\tNormalize appNames'
 	for appName in appNameSet:
 		dicts.appnames[appName] = appId
-		ds['appid_'+appId] = map(lambda x: appName in x.split(',') and 1 or 0, ds['appNames'])
-		dicts.appnameids.append('appid_'+appId)
+		ds.loc[:,'appid_%d' % appId] = map(lambda x: appName in x.split(',') and 1 or 0, ds['appNames'])
+		dicts.appids.append('appid_%d' % appId)
 		appId += 1
-	return ds
+		sys.stdout.write('.')
+	print '\tNormalize over'
+	return ds,dicts
 
 def normalize_field(ds, field_name, dicts, field_limits=None):
 	# remove rows with bad format
 	if not field_limits is None:
+		rows_before = ds.shape[0]
 		ds = ds.loc[ds[field_name].isin(field_limits.keys())]
-		ds[field_name + '_id'] = map(lambda x: field_limits[x], ds[field_name])
+		print 'Records lost for filter %s %d' % (field_name, rows_before - ds.shape[0])
+		ds.loc[:, field_name + '_id'] = map(lambda x: field_limits[x], ds[field_name])
 	####### normalize fields:
 	else:
 		field_id = 0
 		for field_type in ds[field_name].unique():
-			dicts.add_name(field_name, field_id)
+			dicts.add_name(field_name, field_type, field_id)
 			field_id += 1
-		ds[field_name + '_id'] = map(lambda x: dicts.othernames[x], ds[field_name])
+		ds.loc[:, field_name + '_id'] = map(lambda x: dicts.othernames[field_name + "." + x], ds[field_name])
 	dicts.otherids.append(field_name + '_id')
 	return ds
 
